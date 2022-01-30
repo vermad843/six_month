a5c28a1d3d1872cf25824a5a7fecee75caa1b1e2
vermad843 <vishnuverma7524@gmail.com>
2022-01-31 02:18:11 +0530

    Header component

---
+++ src/Header.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+function Header() {
+    return (
+        <div className = "header">
+            <h1>I am a header</h1>
+        </div>
+    )
+}
+
+export default Header;
