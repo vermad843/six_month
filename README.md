23876cab6151f3d31de9805fcb4b9d7519aaad3d
vermad843 <vishnuverma7524@gmail.com>
2022-02-01 09:14:04 +0530

    header__left logos

---
+++ README.md
@@ -0,0 +1,7 @@
+[x] Icons - https://material-ui.com/components/material-icons/
+
+[x] npm install @mui/icons-material
+
+[x] npm install @mui/material @emotion/react @emotion/styled
+
+[x] youtube image - https://upload.wikimedia.org/wikipedia/commons/b/b8/YouTube_Logo_2017.svg
--- src/Header.js
+++ src/Header.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import './Header.css';
-
+import MenuIcon from '@mui/icons-material/Menu';
 
 
 function Header() {
     return (
-        <div className = "header">
+        <div className = 'header'>
             <div className='header__left'>
-                <h1>Logo</h1>
+                <MenuIcon/>
+                <img
+                   className='header__logo'
+                   src = 'https://upload.wikimedia.org/wikipedia/commons/b/b8/YouTube_Logo_2017.svg'
+                   alt = ''
+                 />
             </div>
             <div className='header__input'>
                 <h1>Input</h1>
