6f0993e9e1d6a23ff1203e9c95ec54e8ab617ad8
vermad843 <vishnuverma7524@gmail.com>
2022-02-04 09:13:48 +0530

    recommended component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import './App.css';
 import Header from './Header';
 import Sidebar from './Sidebar';
+import Recommended from './Recommended';
 
 function App() {
   return (
     <div className="App">
       <Header/>
-      <div className='app__page'>
-         <Sidebar/>
-      </div>
+      <Sidebar/> 
+      <Recommended/>
     </div>
   );
 }
---
+++ src/Recommended.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Recommended() {
+  return <div className='recommended'>
+        <h1>I am recommended component</h1>
+  </div>;
+}
+
+export default Recommended;
