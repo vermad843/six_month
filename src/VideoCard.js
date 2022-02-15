d28e6f6e1aaf9da4c4cc7f59f5df8e1a863887ad
vermad843 <vishnuverma7524@gmail.com>
2022-02-15 20:15:27 +0530

    accessing from videocard component

--- src/VideoCard.js
+++ src/VideoCard.js
@@ -2,25 +2,32 @@ import React from 'react';
 import './VideoCard.css';
 import Avatar from '@mui/material/Avatar';
 
-function VideoCard() {
-  return <div className='videoCard'>
-      <img
-        src = "https://i.ytimg.com/vi/4RTte4RA1Sg/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLCOdlLEgQtra4DmRSoMZCCCN8mKmw"
-        className = "videoCard__thumbnail"
-       />
-       <div className='videoCard__info'>
-           <Avatar
-             className = "videoCard__avatar"
+function VideoCard({image,title,channel,views, timestamp, channelImage}) {
+  return (
+      <div className = "videoCard">
+         <img
+            className = "videoCard__thumbnail"
+            src = {image}
+            alt =""
            />
-           <div className='video__text'>
-              <h4>Text</h4>
-              <p>Channel Name</p>
-              <p>
-                view * timestamp
-              </p>
-           </div>
-       </div>
-  </div>;
+          <div className = "videoCard__info">
+              <Avatar
+                  className = "videoCard__avatar"
+                  alt ={channel}
+                  src = {channelImage}
+                />
+                <div className = "video__text">
+                    <h4>{title}</h4>
+                    <p>{channel}</p>
+                    <p>
+                        {views} * {timestamp}
+                    </p>
+                </div>
+          </div>
+      </div>
+  )
 }
+
+
 
 export default VideoCard;
