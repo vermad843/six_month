17600bc9860bcaf5d1732dd218b0a7a77d1a16c4
vermad843 <vishnuverma7524@gmail.com>
2022-02-15 19:51:54 +0530

    videocard

--- README.md
+++ README.md
@@ -7,3 +7,6 @@
 [x] youtube image - https://upload.wikimedia.org/wikipedia/commons/b/b8/YouTube_Logo_2017.svg
 
 [x] [x] Image(github) - https://avatars.githubusercontent.com/u/51009293?s=60&v=4
+
+
+[x] Maninder Image - https://i.ytimg.com/vi/4RTte4RA1Sg/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLCOdlLEgQtra4DmRSoMZCCCN8mKmw
--- src/VideoCard.js
+++ src/VideoCard.js
@@ -1,8 +1,25 @@
 import React from 'react';
+import './VideoCard.css';
+import Avatar from '@mui/material/Avatar';
 
 function VideoCard() {
-  return <div>
-      <h1>I am a videoCard component</h1>
+  return <div className='videoCard'>
+      <img
+        src = "https://i.ytimg.com/vi/4RTte4RA1Sg/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLCOdlLEgQtra4DmRSoMZCCCN8mKmw"
+        className = "videoCard__thumbnail"
+       />
+       <div className='videoCard__info'>
+           <Avatar
+             className = "videoCard__avatar"
+           />
+           <div className='video__text'>
+              <h4>Text</h4>
+              <p>Channel Name</p>
+              <p>
+                view * timestamp
+              </p>
+           </div>
+       </div>
   </div>;
 }
 
