57902563b8f5985855052bdb7382e30069399bce
vermad843 <vishnuverma7524@gmail.com>
2022-02-16 21:09:19 +0530

    videocard.css

--- src/Recommended.js
+++ src/Recommended.js
@@ -1,16 +1,19 @@
 import React from 'react';
 import VideoCard from './VideoCard';
+import './Recommended.css';
 
 function Recommended() {
   return <div className='recommended'>
-        <VideoCard
-          title = "Happy Birthday by Maninder butter"
-          views = "2.3M views"
-          timestamp = "3 days ago"
-          channelImage = "https://avatars.githubusercontent.com/u/51009293?s=60&v=4"
-          channel = "EverythingMusic"
-          image = "https://i.ytimg.com/vi/4RTte4RA1Sg/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLCOdlLEgQtra4DmRSoMZCCCN8mKmw"
-        />
+      <div className='recommended__videos'>
+         <VideoCard
+             title = "Happy Birthday by Maninder butter"
+             views = "2.3M views"
+             timestamp = "3 days ago"
+             channelImage = "https://avatars.githubusercontent.com/u/51009293?s=60&v=4"
+             channel = "EverythingMusic"
+             image = "https://i.ytimg.com/vi/4RTte4RA1Sg/hq720.jpg?sqp=-oaymwEcCNAFEJQDSFXyq4qpAw4IARUAAIhCGAFwAcABBg==&rs=AOn4CLCOdlLEgQtra4DmRSoMZCCCN8mKmw"
+           />
+      </div>
   </div>;
 }
 
